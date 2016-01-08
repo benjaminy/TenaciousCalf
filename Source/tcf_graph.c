8811fcbe9534756cb82ddcd9d3533355267e280c
poopyface97 <john.moody@coloradocollege.edu>
2016-01-07 17:39:13 -0700

    Update tcf_graph.c

--- Source/tcf_graph.c
+++ Source/tcf_graph.c
@@ -1,12 +1,20 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include "cow_trie.c"
 
-typedef struct node_t node_t, *node_p;
-struct node_t {
-    uint32_t val;
-};
-typedef struct edge_t edge_t, *edge_p;
-struct edge_t {
-    node_p pred;
-    node_p ant;
+typedef struct node_or_edge_t node_or_edge_t, *node_or_edge_p;
+
+struct node_or_edge_t {
+    int tag;
+    union {
+        struct {
+            uint32_t label;
+            size_t pred_ct, succ_ct;
+            int *preds, *succs;
+        } node;
+        struct {
+            uint32_t label;
+            int pred, succ;
+        } edge;
+    } _;
 };

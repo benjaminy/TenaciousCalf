65f0d2ea0db0c8765c519c17a805827a5e352a72
poopyface97 <john.moody@coloradocollege.edu>
2016-01-25 20:39:37 -0700

    Update tester.c

--- Source/tester.c
+++ Source/tester.c
@@ -7,7 +7,6 @@
 #include "cow_trie.c"
 
 uint32_t random_int(int min, uint32_t max) {
-    srand(time(NULL));
     return rand() % (max + 1 - min) + min;
 }
 
@@ -56,57 +55,79 @@ uint32_t *random_unique_array(int num_keys, uint32_t max) {
 }
 
 
-val_key_p make_values(uint32_t *key_array, int num_values) {
-    val_key_p val_array = (val_key_p)malloc(num_values * sizeof(val_array[0]));
+node_or_edge_p make_values(uint32_t *key_array, int num_values) {
+    node_or_edge_p val_array = (node_or_edge_p)malloc(num_values * sizeof(val_array[0]));
     for (int i=0; i<num_values; i++) {
-        srand(time(NULL));
-        val_array[i].key_frag = key_array[i];
         //generate tag
         int tag = random_int(0, 1);
-        val_array[i].val.tag = tag;
+        val_array[i].tag = tag;
         if (tag == 0) {
             //generate a node
-            val_array[i].val._.node.label = random_int(0, UINT32_MAX-1);
-            val_array[i].val._.node.pred_ct = (size_t)random_int(0, 50);
-            val_array[i].val._.node.succ_ct = (size_t)random_int(0, 50);
-            val_array[i].val._.node.preds = random_unique_array(val_array[i].val._.node.pred_ct, UINT32_MAX-1);
-            val_array[i].val._.node.succs = random_unique_array(val_array[i].val._.node.succ_ct, UINT32_MAX-1);
+            val_array[i]._.node.label = random_int(0, UINT32_MAX-1);
+            val_array[i]._.node.pred_ct = (size_t)random_int(0, 50);
+            val_array[i]._.node.succ_ct = (size_t)random_int(0, 50);
+            val_array[i]._.node.preds = random_unique_array(val_array[i]._.node.pred_ct, UINT32_MAX-1);
+            val_array[i]._.node.succs = random_unique_array(val_array[i]._.node.succ_ct, UINT32_MAX-1);
         }
         else if (tag == 1) {
             //generate an edge
-            val_array[i].val._.edge.label = random_int(0, UINT32_MAX-1);
-            val_array[i].val._.edge.pred = random_int(0, UINT32_MAX-1);
-            val_array[i].val._.edge.succ = random_int(0, UINT32_MAX-1);
+            val_array[i]._.edge.label = random_int(0, UINT32_MAX-1);
+            val_array[i]._.edge.pred = random_int(0, UINT32_MAX-1);
+            val_array[i]._.edge.succ = random_int(0, UINT32_MAX-1);
         }
     }
     return val_array;
 }
 
-int check_result(cow_trie_p map, uint32_t *keys, val_key_p vals, int result, int size) {
+int check_result(cow_trie_p map, uint32_t *keys, node_or_edge_p vals, int result, int size) {
     for (int i=0;i<size;i++) {
         val_t u;
         cow_trie_lookup(map, keys[i], &u);
+        printf("key: %d\n", keys[i]);
         if (u.tag == 0) {
-            printf("node label == %d", u._.node.label);
-            printf("node label from list == %d\n\n", vals[i].val._.node.label);
+            printf("node label from lookup == %d\n ", u._.node.label);
+            printf("node label from list == %d\n\n", vals[i]._.node.label);
         }
         else if (u.tag == 1) {
-            printf("edge label == %d", u._.edge.label);
-            printf("edge label from list == %d\n\n", vals[i].val._.edge.label);
+            printf("edge label from lookup == %d\n", u._.edge.label);
+            printf("edge label from list == %d\n\n", vals[i]._.edge.label);
+        }
+        else {
+            printf("got a tag that's not 0 or 1 (you made an oops)\n\n");
         }
     }
     return result;
 }
 
 int main() {
-    uint32_t *keys = random_unique_array(2, UINT32_MAX-1);
-    val_key_p vals = make_values(keys, 2);
+    int size = 50000;
+    uint32_t *keys = random_unique_array(size, UINT32_MAX-1);
+    node_or_edge_p vals = make_values(keys, size);
     cow_trie_p test = cow_trie_alloc(0,0);
     test->child_bitmap = 0;
     test->value_bitmap = 0;
-    for (int i=0;i<2;i++) {
-        cow_trie_insert(test, keys[i], vals[i].val, &test);
+    for (int i=0;i<size;i++) {
+        printf("key: %d\n", keys[i]);
     }
-    int result = 0;
-    check_result(test, keys, vals, result, 2);
+    printf("\n");
+    for (int i=0;i<size;i++) {
+        if (vals[i].tag == 0) {
+            printf("val: %d\n", vals[i]._.node.label);
+        }
+        if (vals[i].tag == 1) {
+            printf("val: %d\n", vals[i]._.edge.label);
+        }
+    }
+    printf("\n");
+    for (int i=0;i<size;i++) {
+        if (i == 16) {
+            printf("barry white green beans");
+        }
+        cow_trie_insert(test, keys[i], vals[i], &test);
+        printf("inserted %d\n\n\n", i);
+    }
+    printf("\n");
+    int p = 0;
+    check_result(test, keys, vals, p, size);
+    fflush(stdout);
 }

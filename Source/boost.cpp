f948ed9a93f6408ea8360446fcf9d9dcd1bee61d
poopyface97 <john.moody@coloradocollege.edu>
2016-03-28 13:38:52 -0600

    Added files via upload

--- Source/boost.cpp
+++ Source/boost.cpp
@@ -8,9 +8,24 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/random.hpp>
 #include <stdint.h>
-#include "cow_trie.h"
-#include "tester.h"
+extern "C" {
+    #include "c_t_2.h"
+    #include "tester.h"
+}
 #include <map>
+#include <sys/time.h>
+
+typedef struct two_ids_thing two_ids_thing, *two_ids_thing_p;
+
+#ifdef USE_UINT64_T
+struct two_ids_thing {
+    uint64_t source;
+    uint64_t target;
+    bool operator<(two_ids_thing other) const {
+        return source > other.source;
+    }
+};
+#endif
 
 
 typedef boost::adjacency_list<> Graph;
@@ -20,13 +35,15 @@ typedef boost::property_map<Graph, boost::vertex_index_t>::type IndexMap;
 typedef boost::graph_traits<Graph> GraphTraits;
 typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter;
 
-uint32_t size_walk = 2000000;
-uint32_t *id_array = (uint32_t *)malloc(sizeof(uint32_t)*size_walk);
+uint32_t size_walk = 100000000;
+int_type *id_array = (int_type *)malloc(sizeof(id_array[0])*size_walk);
 uint32_t node_count = 0;
-long long n_count = 2000000;
+long long n_count = 1000000;
 Vertex vt;
 
-int random_walk(Vertex vt, IndexMap index, Graph g) {
+float random_walk(Vertex vt, IndexMap index, Graph g) {
+    struct timeval tval_before, tval_after, tval_result;
+    gettimeofday(&tval_before, NULL);
     for (int i=0;i<size_walk;i++) {
         boost::graph_traits<Graph>::out_edge_iterator out_i, out_end;
         GraphTraits::edge_descriptor e;
@@ -45,9 +62,15 @@ int random_walk(Vertex vt, IndexMap index, Graph g) {
         e = *out_i;
         vt = target(e, g);
     }
-    return 1;
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    printf("Time for theirs: %ld.%06d\n", tval_result.tv_sec, tval_result.tv_usec);
+    float time_elapsed = ((long int)tval_result.tv_usec + (long int)tval_result.tv_sec/1000000)/1000000.0;
+    return time_elapsed;
 }
-int random_walk_c(val_t vt, cow_trie_p g) {
+float random_walk_c(val_t vt, cow_trie_p g) {
+    struct timeval tval_before, tval_after, tval_result;
+    gettimeofday(&tval_before, NULL);
     for (int i=0;i<size_walk;i++) {
         uint32_t e_degree = vt._.node.succ_ct;
         while (e_degree == 0) {
@@ -55,77 +78,112 @@ int random_walk_c(val_t vt, cow_trie_p g) {
             cow_trie_lookup(g, random_vertex, &vt);
             e_degree = vt._.node.succ_ct;
         }
-        uint32_t random_edge = random_int(0, e_degree-1);
-        uint32_t key = vt._.node.succs[random_edge];
+        int random_edge = random_int(0, e_degree-1);
+        int_type key = edge_list_values(vt._.node.succs)[random_edge];
         cow_trie_lookup(g, key, &vt);
         cow_trie_lookup(g, vt._.edge.succ, &vt);
     }
-    return 1;
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    printf("Time for ours: %ld.%06d\n", tval_result.tv_sec, tval_result.tv_usec);
+    float time_elapsed = ((long int)tval_result.tv_usec + (long int)tval_result.tv_sec/1000000)/1000000.0;
+    return time_elapsed;
+}
+
+size_t traverse(cow_trie_p map) {
+    size_t count = 0;
+    count += map->size;
+    for (int i=0;i<count_one_bits(map->child_bitmap);i++) {
+        count += traverse(cow_trie_children(map)[i]);
+    }
+    return count;
 }
 
 int main() {
-  GraphTraits::out_edge_iterator out_i, out_end;
-  GraphTraits::edge_descriptor e;
-  boost::graph_traits<Graph>::adjacency_iterator ai;
-  boost::graph_traits<Graph>::adjacency_iterator ai_end;
-  boost::minstd_rand gen;
-  std::map<uint64_t, uint32_t> m;
-  uint64_t *v_array = (uint64_t *)malloc(sizeof(uint64_t)*(n_count + n_count*3));
-  // Create graph with 100 nodes and edges with probability 0.05
-  double prob = n_count*n_count;
-  prob = n_count*3/prob;
-  Graph g(ERGen(gen, n_count, prob), ERGen(), n_count);
-  std::cout << "done generating\n\n";
-  IndexMap index = get(boost::vertex_index, g);
-  cow_trie_p my_map = cow_trie_alloc(0, 0);
-  uint32_t id = 0;
-  //for each vertex
-  std::cout << "beginning vertex iteration\n\n";
-  std::pair<vertex_iter, vertex_iter> vp;
-  for (vp = vertices(g); vp.first != vp.second; ++vp.first) {
-    Vertex v = *vp.first;
-    boost::tie(out_i, out_end) = out_edges(v, g);
-    uint32_t deg = out_degree(v, g);
-    uint32_t *succs = new uint32_t[deg];
-    uint32_t *preds = new uint32_t[0];
-    val_t my_val = cow_trie_create_val(0, 0, 0, deg, preds, succs, 0, 0);
-    cow_trie_insert(my_map, id, my_val, &my_map);
-    ++id;
-  }
-  std::cout << "vertex iteration done, beginning edge iteration\n\n";
-  boost::graph_traits<Graph>::edge_iterator ei, ei_end;
-  boost::tie(ei, ei_end) = edges(g);
-  std::cout << "id before is" << id << "\n\n"; 
-  for (ei; ei != ei_end; ++ei) {
-    val_t my_val = cow_trie_create_val(1, 0, 0, 0, 0, 0, index[source(*ei, g)], index[target(*ei, g)]);
-    cow_trie_insert(my_map, id, my_val, &my_map);
-    m[index[source(*ei, g)] << 32 | index[target(*ei, g)]] = id;
-    ++id;
-  }
-  std::cout << "id is\n\n" << id << "\n\n";
-  std::cout << "done edge iterating\n\n";
-  std::cout << "putting in successors\n\n";
-  int p = 0;
-  for (vp = vertices(g); vp.first != vp.second; ++vp.first) {
-    Vertex v = *vp.first;
-    val_t my_val;
-    cow_trie_lookup(my_map, p, &my_val);
-    boost::tie(out_i, out_end) = out_edges(v, g);
-    int eindex = 0;
-    for (out_i; out_i != out_end; ++out_i) {
-      e = *out_i;
-      Vertex src = source(e, g), targ = target(e, g);
-      uint32_t id = m[index[src] << 32 | index[targ]];
-      my_val._.node.succs[eindex] = id;
-      ++eindex;
+    GraphTraits::out_edge_iterator out_i, out_end;
+    GraphTraits::edge_descriptor e;
+    boost::graph_traits<Graph>::adjacency_iterator ai;
+    boost::graph_traits<Graph>::adjacency_iterator ai_end;
+    boost::minstd_rand gen;
+
+    std::map<two_ids_thing, uint32_t> m;
+    std::map<int_type, int_type> id_map;
+    std::map<int_type, int_type> self_map;
+
+    size_t count = 0;
+
+    uint64_t *v_array = (uint64_t *)malloc(sizeof(uint64_t)*(n_count + n_count*3));
+    // Create graph with 100 nodes and edges with probability 0.05
+    double prob = n_count*n_count;
+    prob = n_count*3/prob;
+    Graph g(ERGen(gen, n_count, prob), ERGen(), n_count);
+
+    std::cout << "done generating\n\n";
+
+    IndexMap index = get(boost::vertex_index, g);
+    cow_trie_p my_map = cow_trie_alloc(0, 0);
+    my_map->ref_count = 1;
+    my_map->child_bitmap = 0;
+    my_map->value_bitmap = 0;
+    uint32_t id = 0;
+
+    std::cout << "beginning vertex iteration\n\n";
+    std::pair<vertex_iter, vertex_iter> vp;
+    for (vp = vertices(g); vp.first != vp.second; ++vp.first) {
+        Vertex v = *vp.first;
+        boost::tie(out_i, out_end) = out_edges(v, g);
+        uint32_t deg = out_degree(v, g);
+        int_type *succs = new int_type[deg];
+        val_t my_val = cow_trie_create_val(0, deg, succs, 0, 0);
+        int_type my_id = cow_trie_insert(my_map, my_val, &my_map);
+        id_map[id] = my_id;
+        self_map[id] = id;
+        ++id;
     }
-    ++p;
-  }
-  vp = vertices(g);
-  Vertex vt = *vp.first;
-  std::cout << "begin creating walk\n\n";
-  val_t v;
-  cow_trie_lookup(my_map, 0, &v);
-  random_walk_c(v, my_map);
-  //random_walk(vt, index, g);
+
+    std::cout << "vertex iteration done, beginning edge iteration\n\n";
+    boost::graph_traits<Graph>::edge_iterator ei, ei_end;
+    boost::tie(ei, ei_end) = edges(g);
+    std::cout << "id before is" << id << "\n\n"; 
+    for (ei; ei != ei_end; ++ei) {
+        val_t my_val = cow_trie_create_val(1, 0, 0, id_map[index[source(*ei, g)]], id_map[index[target(*ei, g)]]);
+        int_type my_id = cow_trie_insert(my_map, my_val, &my_map);
+        two_ids_thing t;
+        t.source = index[source(*ei, g)];
+        t.target = index[target(*ei, g)];
+        m[t] = id;
+        id_map[id] = my_id;
+        self_map[id] = id;
+        ++id;
+    }
+    std::cout << "id is\n\n" << id << "\n\n";
+    std::cout << "done edge iterating\n\n";
+    std::cout << "putting in successors\n\n";
+    int p = 0;
+    for (vp = vertices(g); vp.first != vp.second; ++vp.first) {
+        Vertex v = *vp.first;
+        val_t my_val;
+        cow_trie_lookup(my_map, id_map[p], &my_val);
+        int_type *succs = edge_list_values(my_val._.node.succs);
+        boost::tie(out_i, out_end) = out_edges(v, g);
+        int eindex = 0;
+        for (out_i; out_i != out_end; ++out_i) {
+            e = *out_i;
+            Vertex src = source(e, g), targ = target(e, g);
+            two_ids_thing t;
+            t.source = index[src];
+            t.target = index[targ];
+            int_type id = m[t];
+            succs[eindex] = id_map[id];
+            ++eindex;
+        }
+        p++;
+    }
+    val_t v;
+    cow_trie_lookup(my_map, 0, &v);
+    Vertex vtest = *vertices(g).first;
+    std::cout << "beginning random traversal\n\n";
+    //float t2 = random_walk_c(v, my_map);
+    float t1 = random_walk(vtest, index, g);
+    std::cout << "end random traversal\n\n";
 }
---
+++ Source/c_t_2.h
@@ -0,0 +1,103 @@
+#ifndef C_T_2_H_
+#define C_T_2_H_
+#define USE_UINT64_T
+
+typedef struct val_t val_t, *val_p;
+
+typedef struct g_node_t g_node_t, *g_node_p;
+
+typedef struct edge_list_t edge_list_t, *edge_list_p;
+
+/* Using the variable-sized struct trick. 
+ * Stores an array of int_type.
+ */
+
+//node fanout
+#ifdef USE_UINT64_T
+typedef uint64_t int_type;
+#else
+typedef uint32_t int_type;
+#endif
+
+
+struct edge_list_t {
+    uint32_t ref_ct;
+    char _[0];
+};
+
+struct g_node_t {
+    uint32_t succ_ct;
+    edge_list_p succs;
+#ifdef COW_INCLUDE_PREDS
+    uint32_t pred_ct;
+    edge_list_p preds;
+#endif
+};
+
+struct g_edge_t {
+    uint32_t pred, succ;
+};
+
+typedef struct g_edge_t g_edge_t, *g_edge_p;
+
+struct val_t {
+    int tag;
+    union {
+        g_node_t node;
+        g_edge_t edge;
+    } _;
+};
+
+typedef int_type vkey_t;
+
+/* Using the variable-sized struct trick.  There are actually two
+ * arrays:
+ * - cow_trie_next_p children[ child_n ]
+ * - val_vkey_t      values[ value_n ]
+ */
+typedef struct cow_trie_t cow_trie_t, *cow_trie_p;
+    struct cow_trie_t
+{
+    int             ref_count;
+    int_type        child_bitmap, value_bitmap;
+    //size of this node
+    int_type        size;
+    //size of this node and everything underneath
+    size_t          tot_size;
+    char            _[0];
+};
+
+void cow_close_edge_list(edge_list_p e);
+
+void cow_trie_close_node(cow_trie_p map);
+
+int cow_trie_lookup(cow_trie_p map, vkey_t key, val_t *v);
+
+int_type cow_trie_insert(cow_trie_p map, val_t val, cow_trie_p *res);
+
+int cow_trie_delete(cow_trie_p map, vkey_t key, cow_trie_p *res);
+
+cow_trie_p *cow_trie_children( cow_trie_p map);
+
+val_t cow_trie_create_val(int tag,
+                          #if COW_INCLUDE_PREDS
+                          size_t pred_ct, 
+                          #endif
+                          size_t succ_ct,
+                          #if COW_INCLUDE_PREDS
+                          int_type *preds,
+                          #endif
+                          int_type *succs,
+                          int_type pred,
+                          int_type succ);
+
+cow_trie_p cow_trie_alloc(int children, int values);
+
+int count_one_bits( int_type bits );
+
+edge_list_p edge_list_alloc(int ct);
+
+int_type *edge_list_values(edge_list_p e);
+
+#endif
+
